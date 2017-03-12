39c4520a179fc4cae951b81c46e7039fb263f3ad
ahhh <1234559900@qq.com>
2017-03-12 16:38:45 +0800

    ahhh

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+window.onload= function () {
+    (function(){
+        window.scrollReveal = new scrollReveal({ reset: true, move: '50px'});
+    })();
+    //var nav_ul=document.getElementById('nav_ul');
+    //var ullis=nav_ul.getElementsByTagName('li');
+    //var secs=document.getElementsByClassName('section');
+    //var timer=null;
+    //for(var j=0;j<ullis.length;j++) {
+    //    ullis[j].index = j;
+    //    ullis[j].onclick = function () {
+    //        var target = secs[this.index].offsetTop;
+    //        //console.log(target);
+    //        clearInterval(timer);
+    //        timer = setInterval(function () {
+    //            var leader = window.pageYOffset || document.documentElement.offsetTop || document.body.offsetTop || 0;
+    //            console.log(leader);
+    //            var step = (target - leader) / 10;
+    //            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+    //            leader = leader + step;
+    //            window.scrollTo(0,leader);
+    //            if (leader === target) {
+    //                clearInterval(timer);
+    //            }
+    //        }, 40);
+    //    };
+    //}
+
+}
+$(function() {
+    $('.to-top').toTop();
+    $('.home').click(function(){
+        $.scrollTo('#sec1',500);
+    });
+    $('.me').click(function(){
+        $.scrollTo('#sec2',800);
+    });
+    $('.skill').click(function(){
+        $.scrollTo('#sec3',1000);
+    });
+    $('.project').click(function(){
+        $.scrollTo('#sec4',1200);
+    });
+    $('.contact').click(function(){
+        $.scrollTo('#sec5',1200);
+    });
+    $(document).ready(function(){
+ $('.loading').fadeOut();
+});
+
+});
